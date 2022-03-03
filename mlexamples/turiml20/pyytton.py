078d5efad7edfd58aed12343dd533e623322123c
Esa Parkkila <esa.parkkila@tuni.fi>
2022-03-03 10:47:00 +0200

    Fix model loading

--- mlexamples/turiml20/pyytton.py
+++ mlexamples/turiml20/pyytton.py
@@ -3,6 +3,11 @@ import time
 import tensorflow as tf
 import turicreate as tc
 
+MODELFILE = 'models/movie_recs.model'
+RECOMMEND_USERS = [1, 30, 13849200]
+
+os.system("echo 'test' >> models/testfile")
+
 print(tf.test.is_gpu_available())
 tc.config.set_num_gpus(1)
 
@@ -14,16 +19,19 @@ print("==============")
 print("START TRAINING")
 print("==============")
 start_time = time.time()
-if os.path.isfile('movie_recs.model'):
-    model = tc.loa_model('movie_recs.model')
+if os.path.exists(MODELFILE):
+    print("Using old model ...")
+    model = tc.load_model(MODELFILE)
 else:
+    print("Creating new model ...")
     model = tc.recommender.create(train, 'userId', 'title', target='rating')
 print("==============")
 print("END TRAINING", (time.time() - start_time))
 print("==============")
 
-model.save('movie_recs.model')
+model.save(MODELFILE)
 
-recommendations = model.recommend(users=[1, 30, 138491])
+recommendations = model.recommend(users=RECOMMEND_USERS)
 recommendations.print_rows(num_rows=30, num_columns=4)
 
+os.system("echo 'complete' >> models/testfile")
